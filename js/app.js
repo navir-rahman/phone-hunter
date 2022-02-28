0c58c4d929d09fd297dc95133da380ba4f5038e9
navir-rahman <navirrahman@gmail.com>
2022-03-01 02:00:15 +0600

    spiner added

--- js/app.js
+++ js/app.js
@@ -4,11 +4,25 @@ function cnl(){
     }
 }
 
+//showallsearh
+const showallsearh= () =>{
+    const show = document.getElementsByClassName('show');
+    const showallbtn = document.getElementById('showallbtn');
+    for (let index = 0; index < show.length; index++) {
+        const el = show[index];
+        el.style.display=""
+    }
+    showallbtn.style.display="none"
+}
+// gloval declaration 
+const single_result= document.getElementById('single_result');
+const spiner= document.getElementById('spiner');
 
 // search phone 
 const searchMobile =() => {
 
-    const seacrh= document.getElementById('search_word').value
+    const seacrh= document.getElementById('search_word').value;
+    spiner.style.display="";
     fetch(`https://openapi.programming-hero.com/api/phones?search=${seacrh}`)
         .then(res => res.json())
         .then(data=> show_result(data));
@@ -16,31 +30,40 @@ const searchMobile =() => {
  // show all result
 const show_result =(data)=>{
     const search_box= document.getElementById('search_box');
-if (data) {
-    //console.log(data.data)
+if (data.data.length > 0) {
+    console.log(data.data)
     search_box.textContent="";
-    for (const phone of data.data) {
-        cnl(phone)
+    single_result.textContent="";
+
+    for (let index = 0; index < data.data.length; index++) {
+        const element = data.data[index];
         const div= document.createElement("div");
-        //div.setAttribute('id',phone.slug);
-        //div.setAttribute('onclick',"single_product(this.id)");
-        div.classList.add("col");
+        //div.setAttribute('id','');
+        if (index >= 20) {
+            div.style.display="none"
+        }
+        div.classList.add("col","show");
         div.innerHTML= `             
         <div class="card">
-        <img width="400px" src="${phone.image}" class="card-img-top" alt="...">
+        <img width="400px" src="${element.image}" class="card-img-top" alt="...">
         <div class="card-body">
-        <h4 class="card-title">${phone.phone_name}</h4>
-        <h5 class="card-title">Brand: ${phone.brand}</h5>
+        <h4 class="card-title">${element.phone_name}</h4>
+        <h5 class="card-title">Brand: ${element.brand}</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div onclick='single_product("${phone.slug}")' class="btn btn-primary">Button</div>
+            <div onclick='single_product("${element.slug}")' class="btn btn-primary">Button</div>
         </div>
-        </div>
+        </div>        
     `;
     search_box.appendChild(div);
     }
+ 
+    spiner.style.display="none";
 }else{
+    spiner.style.display="none";
     search_box.innerText="No Result Found"
 }
+
+
 }
 
 
@@ -57,7 +80,7 @@ function single_product (id){
      const phone= data.data;
      console.log(phone);
 
-     const single_result= document.getElementById('single_result');
+    
 
      const releaseDate=phone.releaseDate || "No releaseDate found";
 
