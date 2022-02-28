3742b0f54d43e5817d55f708ca8b9fde2977bf5f
navir-rahman <navirrahman@gmail.com>
2022-02-28 23:01:24 +0600

    single product search added

--- js/app.js
+++ js/app.js
@@ -13,6 +13,34 @@ const searchMobile =() => {
         .then(res => res.json())
         .then(data=> show_result(data));
 }
+ // show all result
+const show_result =(data)=>{
+    //console.log(data.data)
+    
+    const search_box= document.getElementById('search_box');
+    search_box.textContent="";
+    for (const phone of data.data) {
+        cnl(phone)
+        const div= document.createElement("div");
+        //div.setAttribute('id',phone.slug);
+        //div.setAttribute('onclick',"single_product(this.id)");
+        div.classList.add("col");
+        
+        div.innerHTML= `             
+        <div class="card">
+        <img width="400px" src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h4 class="card-title">${phone.phone_name}</h4>
+        <h5 class="card-title">Brand: ${phone.brand}</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div onclick='single_product("${phone.slug}")' class="btn btn-primary">Button</div>
+        </div>
+        </div>
+    `;
+    search_box.appendChild(div);
+    }
+}
+
 
 //single search
 
@@ -24,30 +52,19 @@ function single_product (id){
 
 //show_single_result
  const show_single_result = (data)=>{
-     console.log(data)
- }
+     const phone= data.data;
+     console.log(phone);
 
-const show_result =(data)=>{
-    //console.log(data.data)
-    
-    const search_box= document.getElementById('search_box');
-    search_box.textContent="";
-    for (const phone of data.data) {
-        cnl(phone)
-        const div= document.createElement("div");
-        div.setAttribute('id',phone.slug);
-        div.setAttribute('onclick',"single_product(this.id)");
-        div.classList.add("col");
-        
-        div.innerHTML= `             
-        <div class="card">
-        <img width="400px" src="${phone.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${phone.phone_name}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        </div>
-    `;
-    search_box.appendChild(div);
-    }
-}
+     const single_result= document.getElementById('single_result');
+
+
+     single_result.innerHTML=`
+     <img src="${phone.image}" class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 class="card-title">${phone.name}</h5>
+       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+       
+     </div>
+     `
+ }
